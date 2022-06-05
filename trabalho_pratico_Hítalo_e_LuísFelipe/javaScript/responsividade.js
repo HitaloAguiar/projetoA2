53c5038ba30a71e55107044c17f3e195965c274f
Hítalo Aguiar <silver.th1226@gmail.com>
2022-06-05 16:44:27 -0300

    ids e classes da minha tentativa de tornar a pagina responsiva

--- trabalho_pratico_Hítalo_e_LuísFelipe/javaScript/responsividade.js
+++ trabalho_pratico_Hítalo_e_LuísFelipe/javaScript/responsividade.js
@@ -1,2 +1,17 @@
-var cabecalho = document.querySelector(".titulo-principal1");
-var corpo = document.querySelector(".corpo");
+/*var nave = document.querySelector(".titulo-principal2");
+var cabecalho = document.getElementById("cabecalho");
+var corpo = document.getElementById("corpo");
+
+var compStylesCorpo = window.getComputedStyle(corpo);
+
+var valueCorpo = compStylesCorpo.getPropertyValue("width");
+
+console.log(valueCorpo);
+
+var compStylesCabecalho = window.getComputedStyle(cabecalho);
+
+var valueCabecalho = compStylesCabecalho.getPropertyValue("width");
+
+console.log(valueCabecalho);
+
+nave.style.width = valueCabecalho;*/
